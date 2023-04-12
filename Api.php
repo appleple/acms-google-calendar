b7a587fc4a5141a75eb262cb7e7c738b5f76f44f
宇井 陸登 <ui@appleple.com>
2023-04-12 16:05:35 +0900

    fix: OAuth認証後、コンフィグのキャッシュクリアがされない問題を修正

--- Api.php
+++ Api.php
@@ -6,6 +6,7 @@ use Acms\Services\Facades\Storage;
 use DB;
 use SQL;
 use Config;
+use Cache;
 use Google_Client;
 use Google_Service_Calendar;
 use Google_Exception;
@@ -93,6 +94,9 @@ class Api
      */
     public function updateAccessToken($accessToken)
     {
+        if (class_exists('Cache')) {
+            Cache::flush('config');
+        }
         $DB = DB::singleton(dsn());
         $RemoveSQL = SQL::newDelete('config');
         $RemoveSQL->addWhereOpr('config_blog_id', BID);
